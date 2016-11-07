aa32d95915947f346744ee51d541f65b90b2c485
hui <hui@VMW10>
2016-11-07 22:50:50 +0800

    new format data importer

--- ShippingMeasure.Db/DataImporter.cs
+++ ShippingMeasure.Db/DataImporter.cs
@@ -16,8 +16,30 @@ namespace ShippingMeasure.Db
 
         private class TankFileRawData
         {
-
+            public string TankName { get; set; }
+            public List<decimal> VInclination { get; } = new List<decimal>();
+            public List<TankFileTrimmingCorrectionLine> TrimmingCorrectionLines { get; } = new List<TankFileTrimmingCorrectionLine>();
+            public List<decimal> HInclination { get; } = new List<decimal>();
+            public List<TankFileListingCorrectionLine> ListingCorrectionLines { get; } = new List<TankFileListingCorrectionLine>();
+            public List<TankFileVolumeLine> VolumeLines { get; } = new List<TankFileVolumeLine>();
         }
 
+        private class TankFileTrimmingCorrectionLine
+        {
+            public decimal Height { get; set; }
+            public List<decimal> Corrections { get; } = new List<decimal>();
+        }
+
+        private class TankFileListingCorrectionLine
+        {
+            public decimal Height { get; set; }
+            public List<decimal> Correction { get; } = new List<decimal>();
+        }
+
+        private class TankFileVolumeLine
+        {
+            public decimal Height { get; set; }
+            public decimal Volume { get; set; }
+        }
     }
 }
